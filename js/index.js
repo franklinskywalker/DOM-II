561cc63d5ba1afa7f934074d2be5d45b07cfaec7
Franklin Cruz <30530051+franklinskywalker@users.noreply.github.com>
2020-02-18 19:48:35 -0500

    img margin

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const imgTran = document.querySelectorAll("img");
+
+imgTran.forEach( x => {
+    x.addEventListener("mouseover", function(event){
+        event.target.style.margin = "20px";
+    })
+})
